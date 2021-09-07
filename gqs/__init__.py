681b62af32329ceda72d9f43e649e8368161ecfb
RestedWickedDev <jesusdperez@gmail.com>
2021-09-06 17:30:02 -0700

    [create] React App
    Created a React Frontend for the Flask Backend

--- gqs/__init__.py
+++ gqs/__init__.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, send_from_directory
+from flask_restful import Api, Resource, reqparse
+from flask_cors import CORS #comment this on deployment
+from api.HelloApiHandler import HelloApiHandler
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='', static_folder='frontend/build')
+CORS(app) #comment this on deployment
+api = Api(app)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/", defaults={'path':''})
+def serve(path):
+    return send_from_directory(app.static_folder,'index.html')
+
+api.add_resource(HelloApiHandler, '/flask/hello')
+
+if __name__ == '__main__':
+    app.run(debug=True)
