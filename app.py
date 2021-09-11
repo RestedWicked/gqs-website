3620c1e7ef4b9844475cd0c7557e19966ca474bf
RestedWickedDev <jesusdperez@gmail.com>
2021-09-10 18:54:45 -0700

    [Restructure] Basic App Created

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def gqs_homepage():
+    "Renders GQS homepage"
+    return render_template('home.html')
